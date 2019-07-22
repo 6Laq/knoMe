f0de94d7a8968ec2e0dce6d628f2c23c3aa903f9
corn rolla <gwooseduck@gmail.com>
2019-07-22 13:56:41 -0700

    nothing useful

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import './components/signIn'
+
+export default class App extends React.Component {
+  
+  render(){
+
+    return (
+      <div className="App">
+        <h2>Hello Wodie</h2>
+      </div>
+    );
+  }
+}
+
+
---
+++ src/components/signIn.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class SignIn extends React.Component{
+  
+  render() {
+    return(
+      <div>Hello Shawty</div>
+    )
+  }
+}
